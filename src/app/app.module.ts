740a667bd08fc1375c1a841725f59d581f8bc436
Altyn <darzeeia@gmail.com>
2020-04-30 15:38:34 +0300

    fix card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {CardComponent } from './card/card.component'
+import {CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component'
 
 @NgModule({ // декоратор, название сущности (синтаксис ts; это f, которая оборачивает класс, делая его модулем)
   declarations: [ // модуль регистрирует компоненты
     AppComponent,
     CardComponent,
+    FormComponent,
   ],
   imports: [ // ...и другие модули
     BrowserModule // этот позволяет работать в браузере
