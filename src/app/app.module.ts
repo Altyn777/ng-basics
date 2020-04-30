a6f8b1141d735c38d7c669e6785a92cff08a9b60
Altyn <darzeeia@gmail.com>
2020-04-30 11:00:17 +0300

    card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CardComponent } from './card/card.component'
 
 @NgModule({ // декоратор, название сущности (синтаксис ts; это f, которая оборачивает класс, делая его модулем)
   declarations: [ // модуль регистрирует компоненты
-    AppComponent
+    AppComponent,
+    CardComponent,
   ],
   imports: [ // ...и другие модули
     BrowserModule // этот позволяет работать в браузере
